1656866f05bf0e78bbd051545a34538bcef024a5
ZJF <zjftd@tju.edu.cn>
2019-03-07 18:33:01 +0800

    homework2

---
+++ Hello_Instantiation/Hello_Instantiation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Instantiation
+{
+    class Hello
+    {
+        public Hello()
+        {
+
+        }
+        public void print(String[] args)
+        {
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.Write(args[i] + " ");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello hello = new Hello();
+            hello.print(args);
+        }
+    }
+}
